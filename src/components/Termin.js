8eb5bf1096b037d8782fefcc4dad106bdeebce40
TheoVitkovskiy <fvitkovski@mail.de>
2017-09-07 19:58:47 +0200

    First quick draft of what the project will look like

---
+++ src/components/Termin.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
+
+class Termin extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <Table columns={4}>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Tag</Table.HeaderCell>
+            <Table.HeaderCell>Was</Table.HeaderCell>
+            <Table.HeaderCell>Wo</Table.HeaderCell>
+            <Table.HeaderCell>Info</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Mittwoch</Table.Cell>
+            <Table.Cell>Chorprobe</Table.Cell>
+            <Table.Cell>{this.props.raum}</Table.Cell>
+            <Table.Cell>{this.props.info}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
+    );
+  }
+}
+
+export default Termin;
