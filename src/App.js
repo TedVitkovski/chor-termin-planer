04dbb0a41cd22e9ee495a0470dfa7b21ef684afc
TheoVitkovskiy <fvitkovski@mail.de>
2017-09-07 19:58:27 +0200

    First quick draft of what the project will look like

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Tab, Header } from 'semantic-ui-react';
+
+import Termin from './components/Termin';
+import './styles/App.css';
+
+const verticalPanes = [
+  { menuItem: '06.09.2017', render: () => <Tab.Pane> <Termin info='alle'  raum='HG 292'/> </Tab.Pane> },
+  { menuItem: '13.09.2017', render: () => <Tab.Pane> <Termin info='nur Frauen' raum='A 132'/> </Tab.Pane> },
+  { menuItem: '20.09.2017', render: () => <Tab.Pane> <Termin info='nur Männer' raum='E 217'/> </Tab.Pane> },
+  { menuItem: '27.09.2017', render: () => <Tab.Pane> <Termin info='alle' raum='E 312'/> </Tab.Pane> },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="ui container" style={{ marginTop: '5em' }}>
+        <Header as='h2' textAlign='center'>
+            <Header.Content>
+              Termin Planer
+            </Header.Content>
+        </Header>
+        <Tab menu={{ fluid: true, vertical: true, tabular: 'left' }} panes={verticalPanes} />
       </div>
     );
   }
