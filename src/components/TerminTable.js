2d5639cdf81fdd23e3d62914b9528b11727565bb
TheoVitkovskiy <fvitkovski@mail.de>
2017-09-18 14:16:19 +0200

    Add surnames to the table

--- src/components/TerminTable.js
+++ src/components/TerminTable.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
 
-import DynamicTableRow from './DynamicTableRow.js';
+import DynamicTableRow from "./DynamicTableRow.js";
 
 class TerminTable extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       users: {}
-    }
+    };
   }
 
   capitalizeName(name) {
@@ -24,43 +23,53 @@ class TerminTable extends Component {
     const basses = [];
 
     for (let i = 0; i < this.props.names.length; i++) {
-      let userName = this.props.names[i];
-
-      let userRealName = this.capitalizeName(userName.slice(0, -1));
-      let userVoice = this.props.userVoices[userName];
-      switch (userVoice) {
-        case 'Sopran' : {
-          soprans.push(userRealName);
-          break;
-        }
-        case 'Alt' : {
-          alts.push(userRealName);
-          break;
-        }
-        case 'Tenor' : {
-          tenors.push(userRealName)
-          break;
-        }
-        case 'Bass' : {
-          basses.push(userRealName)
-          break;
+      for (let j = 0; j < this.props.teilnehmer.length; j++) {
+        let userName = this.props.names[i];
+        let userRealName = this.capitalizeName(userName.slice(0, -1));
+        if (this.props.teilnehmer[j]["vorname"] === userRealName) {
+          let nachname = this.props.teilnehmer[j]["name"];
+          let userFullName = userRealName + " " + nachname;
+          let userVoice = this.props.userVoices[userName];
+          switch (userVoice) {
+            case "Sopran": {
+              soprans.push(userFullName);
+              break;
+            }
+            case "Alt": {
+              alts.push(userFullName);
+              break;
+            }
+            case "Tenor": {
+              tenors.push(userFullName);
+              break;
+            }
+            case "Bass": {
+              basses.push(userFullName);
+              break;
+            }
+          }
         }
       }
     }
-    users['soprans'] = soprans;
-    users['alts'] = alts;
-    users['tenors'] = tenors;
-    users['basses'] = basses;
+    users["soprans"] = soprans;
+    users["alts"] = alts;
+    users["tenors"] = tenors;
+    users["basses"] = basses;
     return users;
-  }
-
-
+  };
 
   render() {
     const users = this.createVoicesToNames();
 
     return (
-      <Table color='orange' size='large' celled padded='normal' textAlign="center" columns={4}>
+      <Table
+        color="orange"
+        size="large"
+        celled
+        padded="normal"
+        textAlign="center"
+        columns={4}
+      >
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Sopran</Table.HeaderCell>
@@ -71,22 +80,29 @@ class TerminTable extends Component {
         </Table.Header>
 
         <Table.Body>
-          <DynamicTableRow users={users} rowNumber={0}/>
-          <DynamicTableRow users={users} rowNumber={1}/>
-          <DynamicTableRow users={users} rowNumber={2}/>
-          <DynamicTableRow users={users} rowNumber={3}/>
-          <DynamicTableRow users={users} rowNumber={4}/>
+          <DynamicTableRow users={users} rowNumber={0} />
+          <DynamicTableRow users={users} rowNumber={1} />
+          <DynamicTableRow users={users} rowNumber={2} />
+          <DynamicTableRow users={users} rowNumber={3} />
+          <DynamicTableRow users={users} rowNumber={4} />
         </Table.Body>
         <Table.Footer>
           <Table.Row>
-            <Table.HeaderCell style={{fontWeight: '700'}}>{this.props.sopran}</Table.HeaderCell>
-            <Table.HeaderCell style={{fontWeight: '700'}}>{this.props.alt}</Table.HeaderCell>
-            <Table.HeaderCell style={{fontWeight: '700'}}>{this.props.tenor}</Table.HeaderCell>
-            <Table.HeaderCell style={{fontWeight: '700'}}>{this.props.bass}</Table.HeaderCell>
+            <Table.HeaderCell style={{ fontWeight: "700" }}>
+              {this.props.sopran}
+            </Table.HeaderCell>
+            <Table.HeaderCell style={{ fontWeight: "700" }}>
+              {this.props.alt}
+            </Table.HeaderCell>
+            <Table.HeaderCell style={{ fontWeight: "700" }}>
+              {this.props.tenor}
+            </Table.HeaderCell>
+            <Table.HeaderCell style={{ fontWeight: "700" }}>
+              {this.props.bass}
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Footer>
       </Table>
-
     );
   }
 }
